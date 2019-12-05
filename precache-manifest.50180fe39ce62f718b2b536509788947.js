3d3c77772ddcd3eed38758a267d0042b3f16b6de
aquaorange-rajeev <rajeev@aquaorange.io>
2019-12-05 19:30:34 +0530

    first commit

---
+++ precache-manifest.50180fe39ce62f718b2b536509788947.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cafebd2e90b9c6df60213ed437239936",
+    "url": "/index.html"
+  },
+  {
+    "revision": "989f7e016c19a53a41f8",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "989f7e016c19a53a41f8",
+    "url": "/static/js/2.b2417c35.chunk.js"
+  },
+  {
+    "revision": "002adfa530514bdfd878",
+    "url": "/static/js/main.42a9947d.chunk.js"
+  },
+  {
+    "revision": "fa31a5a41978e3a89e54",
+    "url": "/static/js/runtime-main.ac815b16.js"
+  }
+]);
